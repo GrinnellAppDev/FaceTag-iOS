639834e182dc39fd299b201c8f771285cf0b1b35
Colin Tremblay <tremblay@grinnell.edu>
2014-01-17 21:33:09 -0600

    Empty Xcode project + Podfile

---
+++ FaceTag/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FaceTag
+//
+//  Created by Colin Tremblay on 1/17/14.
+//  Copyright (c) 2014 GrinnellAppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FaceTag/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FaceTag
+//
+//  Created by Colin Tremblay on 1/17/14.
+//  Copyright (c) 2014 GrinnellAppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
