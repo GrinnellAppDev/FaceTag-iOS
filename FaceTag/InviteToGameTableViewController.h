f7f9e39a0fde1895787a8cdbf5a1f3bfb6871351
Moujaled Maijid <maijid@gmail.com>
2014-01-18 04:11:45 -0500

    InviteGameViewController

---
+++ FaceTag/InviteToGameTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  InviteToGameTableViewController.h
+//  FaceTag
+//
+//  Created by Maijid Moujaled on 1/18/14.
+//  Copyright (c) 2014 GrinnellAppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InviteToGameTableViewController : UITableViewController
+
+@end
